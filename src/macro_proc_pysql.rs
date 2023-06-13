cb0edcc736d43701a80c7f92dfe9cc4170c6f6b0
zxj <zhuxiujia@qq.com>
2023-06-13 17:18:18 +0800

    add example

--- src/macro_proc_pysql.rs
+++ src/macro_proc_pysql.rs
@@ -9,6 +9,7 @@ use init::*;
 use rbatis::executor::Executor;
 use rbatis::rbdc::datetime::DateTime;
 use rbatis::Error;
+use rbatis::rbdc::db::ExecResult;
 use serde_json::json;
 
 /// table
@@ -44,12 +45,31 @@ async fn py_select(
     impled!()
 }
 
+#[py_sql(
+"`delete from biz_activity where delete_flag = 0`
+                  if name != '':
+                    ` and name=#{name}`
+                  if !ids.is_empty():
+                    ` and id in `
+                    ${ids.sql()}"
+)]
+async fn py_exec(
+    rb: &mut dyn Executor,
+    name: &str,
+    ids: &[i32],
+) -> Result<ExecResult, Error> {
+    impled!()
+}
+
 #[tokio::main]
 pub async fn main() {
     let l = fast_log::init(fast_log::Config::new().console()).expect("rbatis init fail");
     //use static ref
     let rb = init_db().await;
     let a = py_select(&mut rb.clone(), "", &[1, 2, 3]).await.unwrap();
+    println!(">>>>>>>>>>>> {}", json!(a));
+
+    let a = py_exec(&mut rb.clone(), "", &[1, 2, 3]).await.unwrap();
     println!(">>>>>>>>>>>> {}", json!(a));
     l.wait();
 }
